0c97ed1fb88b526cbdb3bcf84ad1f65b1757073d
Ronald Cardozo <ronaldcoyo@gmail.com>
2024-08-15 10:36:16 -0500

    Creando app config con routings

---
+++ src/app/signal/signal-ejemplo1/signal-ejemplo1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-ejemplo1',
+  standalone: true,
+  imports: [],
+  templateUrl: './signal-ejemplo1.component.html',
+  styleUrl: './signal-ejemplo1.component.css'
+})
+export  default class SignalEjemplo1Component {
+
+}
---
+++ src/app/signal/signal-routings.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core";
+import path from "node:path";
+import { SignalComponent } from "./signal.component";
+import { Route } from "@angular/router";
+
+
+
+const ROUTES: Route[] = [
+    {
+        path: '',
+        component: SignalComponent,
+        children: [
+            {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: 'signal-ejemplo1'
+         
+            },
+            {
+                path: 'signal-ejemplo1',
+                loadComponent: () =>
+                    import('./signal-ejemplo1/signal-ejemplo1.component')
+            }
+        ]
+    }
+]
+
+export default ROUTES;

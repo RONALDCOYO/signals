31488fa90bd2ebe938ef0b6c00d05ea1488a273f
Ronald Cardozo <ronaldcoyo@gmail.com>
2024-08-16 00:30:16 -0500

    Creando Header y Footer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,5 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(ROUTES), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(ROUTES), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
